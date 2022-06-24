8b4578e1662998ab92f55eca22d2bd8db75d517a
Dylan van Ballegooijen <dillboards@live.nl>
2022-06-24 17:41:51 +0200

    19

--- commands.sh
+++
@@ -1,8 +0,0 @@
-echo "There is a change!"
-echo "Going to changed directory"
-cd /home/farm
-echo "Printing working directory"
-pwd
-echo "restart application"
-systemctl restart farm
-echo "You're up to date and application is restarted"
